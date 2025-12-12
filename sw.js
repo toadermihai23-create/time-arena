bbfeefcefc4a63593040827ca2238dd62f60efe1
toadermihai23-create <toadermihai23@gmail.com>
2025-12-12 21:41:53 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "time-arena-v1";
+const CACHE = "time-arena-v5";
 const ASSETS = [
   "./",
   "./index.html",
@@ -9,20 +9,14 @@ const ASSETS = [
   "./icon-512.png"
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
-});
-
-self.addEventListener("activate", (e) => {
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
